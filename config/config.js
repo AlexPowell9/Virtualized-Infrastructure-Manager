e4495acde267db025d2b2ffa0f0fb0d9227e5043
Alex Powell <apowel24@uwo.ca>
2019-02-16 17:37:41 -0500

    Added a location for tests

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ module.exports = {
     //constants
     ROUTES_DIR: "routes",
     SETUP_ENV_LOCATION: "setup",
+    SETUP_TEST_ENV_LOCATION: "testSetup",
     //functions
     functions: {
         onServerStart: require("./onStart"),
---
+++ test/index.js
@@ -0,0 +1,7 @@
+let run = async () => {
+    const config = require(../config/config);
+    await require(`../config/${config.SETUP_TEST_ENV_LOCATION}`)();
+
+}
+
+run();
