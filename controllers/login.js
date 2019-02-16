d36d5d64dd343e54a175173720d5940912bdcb62
Alex Powell <apowel24@uwo.ca>
2019-02-16 18:04:04 -0500

    Added login controller, needs auth and token return

---
+++ controllers/login.js
@@ -0,0 +1,29 @@
+module.exports = {
+    validate: (req, res, next) {
+        if(!req.username || req.password){
+            return this.responses.missingFields(res);
+        }
+        next();
+    },
+    authenticate: (req, res, next){
+            
+    },
+    generateToken: (req, res, next){
+             
+    }
+    responses: {
+        missingFields: (res, fields) {
+            if(fields){
+                let message = fields.reduce((acc, curr) => {
+                    acc = acc + curr;
+                }, "missing fields: ");
+                return this.badRequest(res, message);
+            } 
+            return this.badRequest(res);
+        },
+        badRequest: (res, message) {
+            message = message || "bad request";
+            res.status(400).json(message);
+        }
+    }
+}
---
+++ routes/login.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const config = require("../config/config");
+
+let router = express.Router();
+
+const loginControllers = require(`../${config.CONTROLLER_LOCATION}/login`);
+
+router.post("/", [loginControllers.validateInput, 
+    loginControllers.authenticate,
+    loginControllers.generateToken
+)];
