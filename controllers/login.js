f1bd0cd5356e3403dd6367d3d02e1edb1e995dfe
Alex Powell <apowel24@uwo.ca>
2019-02-16 19:48:03 -0500

    Added scrypt to the login controllers, also a token generator, right now its a random string but in the future it could be a jwt

--- controllers/login.js
+++ controllers/login.js
@@ -1,3 +1,21 @@
+const jwt = require("./jwt");
+const crypto = require("crypto");
+const scrypt = requrie("scrypt");
+const config = require("../config/config");
+const TOKEN = require(`../${config.MODEL_DIR}/token.js`);
+const scryptParameters = scrypt.paramsSync(0.1);
+const USER = require(`../${config.MODEL_DIR}/user.js`)
+
+const authenticatePassword(password, user){
+    try{
+        if(!user || !password)return false
+        return scrypt.verifyKdfSync(password, user.password);
+    }
+    catch(err) {
+        return err;
+    }
+}
+
 module.exports = {
     validate: (req, res, next) {
         if(!req.username || req.password){
@@ -5,11 +23,20 @@ module.exports = {
         }
         next();
     },
-    authenticate: (req, res, next){
-            
+    authenticate: async (req, res, next){
+        let user = await user.findOne({
+            username: req.username     
+        });
+        if(!user)return this.responses.noUser(res);
+        if(authenticatePassword(req.password, user)){
+            res.locals.user = user;
+            next();
+        };
+        else return this.responses.incorrectPassword(res);
     },
     generateToken: (req, res, next){
-             
+        token = crypto.RandomBytes(64); 
+
     }
     responses: {
         missingFields: (res, fields) {
@@ -24,6 +51,12 @@ module.exports = {
         badRequest: (res, message) {
             message = message || "bad request";
             res.status(400).json(message);
+        },
+        noUser: (res){
+            res.status(400).json("user does not exist");
+        },
+        incorrectPassword: (res) {
+            res.status(400).json("incorrect login");
         }
     }
 }
