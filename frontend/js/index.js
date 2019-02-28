3f97c33aea9bec56cd79af290414b7c985da537d
Christian Yores <christianyores@gmail.com>
2019-02-28 17:17:37 -0500

    added ajax call

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,15 @@
 function login() {
     console.log('logging in');
     // api call
+    $.ajax({
+        url: "localhost:8082/api/login",
+        context: document.body
+    })
+    .done(function(data) {
+        console.log(data);
+    })
+    .fail(function(data) {
+        console.log('error');
+    });
     window.location.replace('dashboard.html');
 }
